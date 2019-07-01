3104aaf8a86df2157895c3cea9e0f590128cd2f7
Holden Beale <itdbealh@aikencounty.net>
2019-07-01 12:36:00 -0400

    image test done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import SimpleList from './components/simpleList.js'
 import OverFlowImage from './components/overflowImage.js'
+import Types from './components/types.js'
 
 function App() {
   return (
     <div className="App">
       <SimpleList />
       <OverFlowImage />
+      <Types />
     </div>
   );
 }
